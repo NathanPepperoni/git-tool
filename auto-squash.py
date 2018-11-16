c12f16b13a6a4b0c196e64dbad0dbfa5645e768f
Pettorini <NP046622@cerner.net>
2018-11-16 11:49:45 -0600

    Changed to using git config instead of directly replacing editor

--- auto-squash.py
+++ auto-squash.py
@@ -1,22 +1,47 @@
 import subprocess, os
 
-os.chdir("C:\\dummy-repo\\")
+AUTO_SEQUENCE_EDITOR = '"sed -i -e \'1 ! s/pick/pick/g\'"'
+DEFAULT_EDITOR = '"vim"'
+PREVIOUS_EDITOR = DEFAULT_EDITOR
 
-def add():
-    process = subprocess.Popen(["git", "add", "-u"], stdout=subprocess.PIPE, shell=True)
-    output = process.communicate()[0]
+def makeBackupBranch():
+    output = makeShellCall(['git', 'branch', '-D', 'autosquash_save_branch'])
+    print(output)
+    output = makeShellCall(['git', 'branch', 'autosquash_save_branch'])
     print(output)
 
-def commit():
-    process = subprocess.Popen(["git", "commit", "-m", "s"], stdout=subprocess.PIPE, shell=True)
-    output = process.communicate()[0]
-    print(output)
-
-def rebase():
-    process = subprocess.Popen(["git", "rebase", "-i", "HEAD"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-    process.communicate("")
-    output = process.communicate()
+def setSequenceEditor():
+    output = makeShellCall(['git', 'config', 'sequence.editor', [AUTO_SEQUENCE_EDITOR]])
     print(output)
     
+def getCurrentEditor():
+    print(makeShellCall(['git', 'config', '--list']))
+    print(os.getcwd())
+    
+def revertSequenceEditor():
+    makeShellCall(['git', 'config', 'sequence.editor', [PREVIOUS_EDITOR]])
+    
+def getLogEntries():
+    entries = []
+    output = makeShellCall(["git", "log"])
+    for s in str(output[0]).split('commit '):
+        if (s[:4] == '    '):
+            entries.append(s[4:])
+    return entries
 
-rebase()
+def makeShellCall(call):
+    process = subprocess.Popen(call, stdout=subprocess.PIPE, shell=True)
+    return process.communicate()
+
+class LogEntry:
+    def __init__(self, commit_hash, author, merge_commit, commit_message):
+        self.commit_hash = commit_hash
+        self.author = author
+        self.merge_commit = merge_commit 
+        self.commit_message = commit_message
+        
+    def __init__(self, commit_hash, author, commit_message):
+        self.__init__(self, commit_hash, author, False, commit_message)
+
+os.chdir("C:\\dummy-repo")
+getLogEntries()
--- editor.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import sys
-
-print("args are: ")
-print(str(sys.argv))
-
-
